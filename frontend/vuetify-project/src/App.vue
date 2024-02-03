df5bb25bf8caca68c2f0cffc3e29e8bb886b649f
Daniel Selga <danielselga@gmail.com>
2024-02-03 16:45:23 -0300

    fix: start front-project with vuetify

---
+++ frontend/vuetify-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <Form />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
---
+++ frontend/vuetify-project/src/components/Form.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+      <v-form @submit.prevent>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="firstName"
+              :rules="rules"
+              label="First name"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="firstName"
+              :rules="rules"
+              label="First name"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="firstName"
+              :rules="rules"
+              label="First name"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn type="submit" @click="test" block class="mt-2">Submit</v-btn>
+      </v-form>
+  </v-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    firstName: "",
+    rules: [
+      (value) => {
+        if (value) return true;
+        return "You must enter a first name.";
+      },
+    ],
+  }),
+  methods: {
+    test() {
+      console.log("submit 1");
+    },
+  },
+};
+</script>
